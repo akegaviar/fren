9e9edaa38dfad2db6d15d33e27120116e6149275
Ake Gaviar <evkonst@gmail.com>
2022-04-14 20:49:31 +0800

    hi2

--- main.js
+++ main.js
@@ -1,26 +1,37 @@
 /* Moralis init code */
-const serverUrl = "https://hub6hxdaysas.usemoralis.com:2053/server";
-const appId = "7AEttH8Ma44UhehjSC2dgrdv0kRaeGnQ3SldZb9x";
+// REPLACE THE BELOW SERVER_URL and APP_ID with your own
+const serverUrl = "https://hpz4yq50hr8y.usemoralis.com:2053/server";
+const appId = "FaLY0U96izeaTHPkmvxHUq87YIejSYU0KMBiHS5M";
 Moralis.start({ serverUrl, appId });
 
+/* Authentication code */
 async function login() {
   let user = Moralis.User.current();
   if (!user) {
     user = await Moralis.authenticate({
-      signingMessage: "Log in using Moralis",
+      signingMessage: "Log in using Moralis"
     })
       .then(function (user) {
         console.log("logged in user:", user);
-        console.log(user.get("ethAddress"));
+        let ethAddress = user.get("ethAddress");
+        document.getElementById(
+          "myethAddress"
+        ).textContent = `Logged in Eth Address ${ethAddress}`;
       })
       .catch(function (error) {
         console.log(error);
       });
+  } else {
+    let ethAddress = user.get("ethAddress");
+    document.getElementById(
+      "myethAddress"
+    ).textContent = `Already Logged in ${ethAddress}`;
   }
 }
 
 async function logOut() {
   await Moralis.User.logOut();
+  document.getElementById("myethAddress").textContent = "";
   console.log("logged out");
 }
 
